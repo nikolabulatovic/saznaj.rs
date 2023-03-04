b72b5126d8896b214a147c4648fd951e475ee72c
Nikola Bulatovic <nikolabulatovic@nmb.local>
2023-03-04 19:42:06 +0100

    feat: add top section with double images and text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import { Box, Typography } from "@mui/material";
-import { BlockQuote, YoutubeEmbed } from "components";
+import { BlockQuote, DoubleImage, YoutubeEmbed } from "components";
 import { fonts } from 'utils';
+
+const darkColorParagraph = {
+  color: '#646464',
+  fontFamily: 'Bio Sans',
+}
 
 export default function HomePage() {
   return (
@@ -19,6 +24,28 @@ export default function HomePage() {
       </Typography>
 
       <Typography variant='h1'>Ovo ne žele da saznaš.</Typography>
+
+      <DoubleImage
+        url1='https://i.imgflip.com/1knrg9.jpg'
+        url2='https://i.imgflip.com/1knrg9.jpg'
+      />
+
+      <Typography sx={darkColorParagraph}>
+        U industrijama u kojima se životinje iskorišćavaju, nikad nam nije
+        prikazana istina o njihovim životima.
+      </Typography>
+
+      <DoubleImage
+        url1='https://i.imgflip.com/1knrg9.jpg'
+        url2='https://i.imgflip.com/1knrg9.jpg'
+      />
+
+      <Typography sx={darkColorParagraph}>
+        Baš kao tvoj pas ili mačka, životinje u industrijama mesa, mleka i
+        jaja osećaju bol i ne žele da budu nepravedno ubijene.{' '}
+        <strong>Životinje nisu naše da sa njima radimo šta god hoćemo</strong>
+        .
+      </Typography>
 
       <YoutubeEmbed embedId="YnONLDo6PWI" />
 
