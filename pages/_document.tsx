7171e03d3bbb1af638451a5cd6fbf2bea9657b14
Slobodan Borisavljevic <slobodan.borisavljevic.boba@gmail.com>
2023-03-25 18:21:44 +0100

    Disable right click

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,20 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import { colors } from 'utils';
 
+const scriptTxt = `
+(function () {
+  document.addEventListener(
+    'contextmenu',
+    event => event.preventDefault()
+  );
+})();
+`
+
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <script dangerouslySetInnerHTML={{ __html: scriptTxt }}/>
         <meta name="theme-color" content="#272020"/>
       </Head>
       <body
