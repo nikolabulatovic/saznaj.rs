478afbfba5d3a00cb833a33e75dac5ba43d364d5
nikolabulatovic <bulatovic_nikola@yahoo.com>
2023-04-08 09:53:48 +0000

    Deploying to gh-pages from @ nikolabulatovic/saznaj.rs@4f3297ed8c8b9e82083f9f8394347a77368c6a7e 🚀

---
+++ _next/static/plqRKTld3Zq9uus9qb7_R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/203-fd67c163d46e3ce9.js","static/css/2cfd3e4037da7c87.css","static/chunks/pages/index-0cb64a2ff32990c3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
